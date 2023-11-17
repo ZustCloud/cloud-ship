a254132050dfcfd50efad036c7e1070df806c29b
chenmh0901 <chenmh0901@foxmail.com>
2023-11-17 10:37:50 +0800

    表单优化

--- app/components/userForm.vue
+++ app/components/userForm.vue
@@ -2,196 +2,56 @@
 	<view>
 		<div class="content">
 			<div class="photo"><uni-file-picker class="photo" :imageStyles="imageStyle" limit="1" file-mediatype="image"></uni-file-picker></div>
-			<uni-forms-item v-for="item in formItemList" :label="item.label">
+			<uni-forms-item v-for="item in USER_DETAIL_FORM_FIELDS" :label="t[item]">
 				<uni-easyinput
 					class="normalInfo"
-					v-model="item.value"
-					:placeholder="item.placeholder"
-					:styles="item.style"
-					:disabled="item.disabled"
-					:placeholderStyle="item.phstyle"
-					:type="item.type"
+					v-model="userDetailForm[item]"
+					:placeholder="`请输入您的${t[item]}`"
+					:styles="props.mode === 'edit' ? editStyles : inputstyle"
+					:disabled="props.mode !== 'edit'"
 				></uni-easyinput>
 			</uni-forms-item>
-			<button class="changeButton" @click="submitInfo" v-if="!isEdit">保存</button>
+			<uni-forms-item>
+				<uni-easyinput
+					class="normalInfo"
+					v-model="userDetailForm['resume']"
+					:placeholder="`请输入您的${t['resume']}`"
+					:styles="props.mode === 'edit' ? editStyles : inputstyle"
+					:disabled="props.mode !== 'edit'"
+				></uni-easyinput>
+			</uni-forms-item>
+			<button class="changeButton" @click="submitInfo" v-if="!props.mode === 'edit'">保存</button>
 		</div>
 	</view>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, getCurrentInstance, inject } from 'vue';
-import { formItem } from './user-form/types';
-import { inputstyle, editstyle, placeholderstyle, IdefaultStyle } from './user-form/constants';
+import { ref, Ref } from 'vue';
+import { USER_DETAIL_FORM_FIELDS, UserDetail } from './user-form/types';
+import t from './user-form/lang';
 
-var isEdit = ref(true);
-var display = ref(false);
-var showStyle: IdefaultStyle = inputstyle;
-const placeholderStyle = 'font-size:16px';
-const init = () => {
-	if (props.type === 'detailed') {
-		display.value = true;
-		// imageStyle.value = DimageStyle;
-	}
-	if (props.mode === 'edit') {
-		isEdit.value = false;
-		showStyle = editstyle;
-	}
-};
-init();
-const formItemList: formItem[] = [
-	{
-		label: '姓名：',
-		type: 'text',
-		value: ref('').value,
-		placeholder: '请输入你的姓名',
-		style: showStyle,
-		disabled: isEdit.value,
-		phstyle: placeholderstyle
-	},
-	{
-		label: '班级：',
-		type: 'text',
-		value: '',
-		placeholder: '请输入你的班级',
-		style: showStyle,
-		disabled: isEdit.value,
-		phstyle: placeholderstyle
-	},
-	{
-		label: '性别：',
-		type: 'text',
-		value: '',
-		placeholder: '请输入你的性别',
-		style: showStyle,
-		disabled: isEdit.value,
-		phstyle: placeholderstyle
-	},
-	{
-		label: '手机号：',
-		type: 'text',
-		value: '',
-		placeholder: '请输入你的手机号',
-		style: showStyle,
-		disabled: isEdit.value,
-		phstyle: placeholderstyle
-	},
-	{
-		label: '生日：',
-		type: 'text',
-		value: '',
-		placeholder: '请输入你的出生年份',
-		style: showStyle,
-		disabled: isEdit.value,
-		phstyle: placeholderstyle
-	},
-	{
-		label: '邮箱：',
-		type: 'text',
-		value: '',
-		placeholder: '请输入你的邮箱号',
-		style: showStyle,
-		disabled: isEdit.value,
-		phstyle: placeholderstyle
-	},
-	{
-		label: '微信：',
-		type: 'text',
-		value: '',
-		placeholder: '请输入你的微信号',
-		style: showStyle,
-		disabled: isEdit.value,
-		phstyle: placeholderstyle
-	},
-	{
-		label: 'QQ：',
-		type: 'text',
-		value: '',
-		placeholder: '请输入你的QQ号',
-		style: showStyle,
-		disabled: isEdit.value,
-		phstyle: placeholderstyle
-	},
-	{
-		label: '个人简介：',
-		type: 'textarea',
-		value: '',
-		placeholder: '介绍一下自己吧~~~',
-		style: showStyle,
-		disabled: isEdit.value,
-		phstyle: placeholderstyle
-	}
-];
-const props = defineProps({
-	userId: {
-		type: String,
-		default: '',
-		required: false
-	},
-	password: {
-		type: String,
-		default: '',
-		required: false
-	},
-	type: {
-		type: String,
-		default: '',
-		required: false
-	},
-	mode: {
-		type: String,
-		default: '',
-		required: false
-	}
+interface IProps {
+	userId: string;
+	password: string;
+	mode: 'edit' | null;
+}
+const props = defineProps<IProps>();
+
+const userDetailForm: Ref<UserDetail> = ref({
+	name: 'string',
+	phone: 'string',
+	email: 'string',
+	sex: 'string',
+	classNo: 'string',
+	birthday: 'string',
+	wechat: 'string',
+	QQ: 'string',
+	resume: 'string'
 });
 
-// const getInfo = () => {
-// 	uni.request({
-// 		url: `http://localhost:3000/user/${props.userId}`,
-// 		method: 'GET',
-// 		success: res => {
-// 			console.log(res.data.data.details);
-// 			var details = res.data.data.details;
-// 			name.value = details.name;
-// 			classNo.value = details.class;
-// 			sex.value = details.sex;
-// 			phoneNum.value = details.phoneNum;
-// 			birthday.value = details.birthday;
-// 			email.value = details.email;
-// 			wechat.value = details.wechat;
-// 			QQ.value = details.QQ;
-// 			resume.value = details.resume;
-// 		}
-// 	});
-// };
-// getInfo()
-
-const classValue = ref('');
-const sexValue = ref('');
-//班级选择器值
-const classList = ref([
-	{
-		value: '数字媒体技术211班',
-		text: '数字媒体技术211班'
-	},
-	{
-		value: '数字媒体技术212班',
-		text: '数字媒体技术212班'
-	}
-]);
-// 性别选择器值
-const sexList = ref([
-	{
-		value: '男',
-		text: '男'
-	},
-	{
-		value: '女',
-		text: '女'
-	}
-]);
-
 //输入框可编辑的样式
-const editStyles = {};
+const editStyles = ref({});
+
 //头像上传缩略样式
 const imageStyle = ref({
 	width: '100',
@@ -202,43 +62,9 @@ const imageStyle = ref({
 		width: '2px'
 	}
 });
-//头像上传细节样式
-const DimageStyle = {
-	width: '160',
-	height: '200',
-	border: {
-		color: '#e3e3e3',
-		width: '2px'
-	}
-};
 
-const sexChange = e => {
-	sexValue.value = e;
-	console.log(sexValue.value);
-};
-const classChange = e => {
-	classValue.value = e;
-	console.log(classValue.value);
-};
 const submitInfo = () => {
-	if (type != 'detailed' && mode === 'edit') {
-		uni.request({
-			url: 'http://localhost:3000/auth/update',
-			method: 'POST',
-			data: {
-				id: props.userId,
-				password: props.password,
-				details: {
-					name: name.value,
-					class: classValue.value,
-					sex: sexValue.value,
-					phoneNum: phoneNum.value
-				},
-				mixin: 'asdf'
-			},
-			success: res => {}
-		});
-	}
+	console.log('hello');
 };
 </script>
 
@@ -260,17 +86,9 @@ const submitInfo = () => {
 	align-items: center;
 }
 
-.name {
-	width: 100%;
-	margin-top: 50px;
-}
-
-.requireInfo {
-	width: 100%;
-}
-
 .normalInfo {
 	width: 100%;
+	background-color: yellow;
 }
 
 .changeButton {
