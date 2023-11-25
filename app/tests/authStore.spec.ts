e9d7b620c2cb927e98a3f476bb3a5a2709d40d37
Nesb01t <nesb01t@163.com>
2023-11-26 03:41:42 +0800

    uni-app 移除单元测试

--- app/tests/authStore.spec.ts
+++
@@ -1,6 +0,0 @@
-import {useauthStore} from "../store/auth";
-
-test('我测试求和', () => {
-  const authStore = useauthStore()
-  console.log(authStore.id)
-})
--- app/tests/sum.spec.js
+++
@@ -1,7 +0,0 @@
-test('我测试求和', () => {
-	expect(sum(1, 10)).toEqual(11)
-})
-
-const sum = (a, b) => {
-	return a + b
-}
